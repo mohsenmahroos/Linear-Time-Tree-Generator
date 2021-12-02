650346c979e422eb104443aed049446a9408b4f2
mohsenmahroos <mohsenmahroos@msn.com>
2021-12-02 04:56:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 using namespace std;
 
 int main() {
-    random_device device;
-    mt19937_64 random(device());
     size_t N;
     cin >> N;
-    for (auto [u,v]: undirected_tree(N,random))
+    for (auto [u,v]: undirected_tree(N))
         cout << u << ' ' << v << endl;
     return 0; }
