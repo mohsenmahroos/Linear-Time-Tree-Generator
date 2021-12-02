3a45b49ab487d6ec58230f70f6d6b670a77736a3
mohsenmahroos <mohsenmahroos@msn.com>
2021-12-02 03:36:53 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "utree.hpp"
+#include<iostream>
+using namespace std;
+
+int main() {
+    random_device device;
+    mt19937_64 random(device());
+    size_t N;
+    cin >> N;
+    for (auto [u,v]: undirected_tree(random,N))
+        cout << u << ' ' << v << endl;
+    return 0; }
