61cbc8c4db363265838269aa3b35a57426892853
mohsenmahroos <mohsenmahroos@msn.com>
2021-12-02 13:00:40 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "utree.hpp"
-#include <iostream>
+#include<iostream>
 using namespace std;
 
 int main() {
+    std::random_device device;
+    std::mt19937_64 random(device());
     size_t N;
     cin >> N;
-    for (auto [u,v]: undirected_tree(N))
+    for (auto [u,v]: undirected_tree(N,random))
         cout << u << ' ' << v << endl;
     return 0; }
